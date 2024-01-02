62a94a71e3eec63e119eb810a19f80209122d110
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-02 21:41:14 +0300

    default controller eklendi

---
+++ DapperApp/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using DapperApp.DapperContext;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Immutable;
+
+namespace DapperApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly Context _context;
+
+        public DefaultController(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string query = "Select * From Project";
+            var connection=_context.CreateConnection();
+            var values= await connection.QueryAsync<>(query);
+            return View(values.ToList());
+        }
+    }
+}
